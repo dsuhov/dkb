6fbd6dc542415cc06896a4ff894ade9183d2a8be
Helen <liaforweb@gmail.com>
2019-11-21 22:17:25 +0200

    popups 1-2

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -4,3 +4,4 @@ import "%modules%/top-screen/top-screen";
 import "%modules%/work-order/work-order";
 import "%modules%/s-features/s-features";
 import "%modules%/form-side/form-side";
+import "%modules%/popups/popups";
