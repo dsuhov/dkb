05b6388f317fc634cbb2c42b34f8898f66503558
Dmitriy Sukhov <dstujev@gmail.com>
2019-11-24 18:22:28 +0300

    add team section

--- src/blocks/modules/work-order/work-order.js
+++ src/blocks/modules/work-order/work-order.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 
 $(document).ready(function() {
-  if (window.matchMedia('(max-width: 375px)').matches) {
+  if (window.matchMedia('(max-width: 1110px)').matches) {
     $('.work-order__title br').replaceWith(' ');
     $('.worder-item__content-block br').replaceWith(' ');
   }
--- src/js/scrollActions/scrollActionsMain.js
+++ src/js/scrollActions/scrollActionsMain.js
@@ -1,6 +1,9 @@
 import $ from "jquery";
 import headerPin from "./header-pin";
+import form1Reveal from "./form1Reveal";
+
 
 $(document).ready(function() {
   headerPin();
+  form1Reveal();
 });
